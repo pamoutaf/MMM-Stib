15a4c2fec68e5af60c5dd2a891d9790623c93a82
Paula Moutafian <ceciliavelezmeija@gmail.com>
2024-02-07 15:38:25 +0100

    Added time for second bus arriving

--- MMM-Stib.js
+++ MMM-Stib.js
@@ -2,12 +2,12 @@ Module.register("MMM-Stib", {
 	expectedArrivalTimes: [],
   
 	getDom: function () { //create the different cells
-	  var wrapper = document.createElement("div");
+		var wrapper = document.createElement("div");
   
-	  var table = document.createElement("table");
-	  table.className = "stib-table small";
+		var table = document.createElement("table");
+		table.className = "stib-table small";
   
-	  this.expectedArrivalTimes.forEach(entry => {
+		this.expectedArrivalTimes.forEach(entry => {
 		var row = document.createElement("tr");
 		
 		var lineIDCell = document.createElement("td");
@@ -29,11 +29,18 @@ Module.register("MMM-Stib", {
 		etaCell.className = "estimated-time-arrival";
 		etaCell.innerHTML = "";
 		row.appendChild(etaCell);
-  
+		
+		// time diff 1
 		var diffCell = document.createElement("td");
 		diffCell.className = "time-difference";
 		diffCell.innerHTML = entry.timeDifference + " min";
 		row.appendChild(diffCell);
+
+		// time diff 2
+		var diffCell2 = document.createElement("td");
+		diffCell2.className = "time-difference2";
+		diffCell2.innerHTML = entry.timeDifference2 + " min";
+		row.appendChild(diffCell2);
   
 		table.appendChild(row);
 	  });
@@ -42,17 +49,19 @@ Module.register("MMM-Stib", {
 	  return wrapper;
 	},
   
-	processData: function (station, expectedArrivalTime, lineid) { //store data from the fetch function
+	processData: function (station, expectedArrivalTime, expectedArrivalTime2, lineid) { //store data from the fetch function
 	  var entry = {
 		direction: station.direction,
 		expectedArrivalTime: expectedArrivalTime,
+		expectedArrivalTime2: expectedArrivalTime2,
 		timeDifference: 0,
+		timeDifference2: 0,
 		lineId: lineid,
 	  };
 	  var date = new Date();
 	  var time = date.getMinutes();
 	  entry.timeDifference = entry.expectedArrivalTime - time; // gets the eta of bus/tram
-  
+	  entry.timeDifference2 = entry.expectedArrivalTime2 - time; // gets the second eta of bus/tram
 	  this.expectedArrivalTimes.push(entry);
 	},
   
@@ -79,15 +88,17 @@ Module.register("MMM-Stib", {
 			const result = resultsArray[0];
 			console.log("print result:",result)
 			const passingTimesArray = JSON.parse(result.passingtimes);
-			const sanitize_input = passingTimesArray.map(item => item.expectedArrivalTime);
-			const real_time_bus = sanitize_input.slice(0, 1);
+			const real_time_bus = passingTimesArray[0].expectedArrivalTime; //first time of arrival
+			const real_time_bus2 = passingTimesArray[1].expectedArrivalTime; //second time of arrival
+			
+			// Set time for bus arrival times
 			const d = new Date(real_time_bus);
+			const d2 = new Date(real_time_bus2);
 			const expectedArrivalTime = d.getMinutes();
-			//console.log("results array", result)
-			//console.log("Testing arrival time",expectedArrivalTime)
+			const expectedArrivalTime2 = d2.getMinutes();
+
 			const lineid = [... new Set(passingTimesArray.map(item => item.lineId))];
-			console.log("line id is:", lineid);
-			self.processData(station, expectedArrivalTime, lineid);
+			self.processData(station, expectedArrivalTime, expectedArrivalTime2, lineid);
 			self.updateDom();
 		})
 		.catch(error => {
